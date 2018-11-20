67e6aae91aa5d84192b44225b7531b4394fb0810
Gina Liu <43168953+gt245@users.noreply.github.com>
2018-11-20 03:45:29 -0500

    Update sketch.js

--- Exercise 10:30:18/sketch.js
+++ Exercise 10:30:18/sketch.js
@@ -1,14 +1,14 @@
-var mySquare1;
-var mySquare2;
-var mySquare3;
+var myBall1;
+var myBall2;
+var myBall3;
 
 function setup() {
 
   createCanvas(500,500);
 
-  mySquare1 = new Square(30, 120, 1, 5, 30);
-  mySquare2 = new Square(190, 260, 3, 3, 50);
-  mySquare3 = new Square(420, 450, 5, 1, 40);
+  myBall1 = new Ball(30, 120, 1, 5, 30);
+  myBall2 = new Ball(190, 260, 3, 3, 50);
+  myBall3 = new Ball(420, 450, 5, 1, 40);
     
 }
 
@@ -17,19 +17,19 @@ function setup() {
 function draw() {
   background(210);
 
-  mySquare1.move();
-  mySquare1.display();
+  myBall1.move();
+  myBall1.display();
 
-  mySquare2.move();
-  mySquare2.display();
+  myBall2.move();
+  myBall2.display();
 
-  mySquare3.move();
-  mySquare3.display();
+  myBall3.move();
+  myBall3.display();
 
 
 }
 
-function Square(myX, myY, myXVel, myYVel, mySize){
+function Ball(myX, myY, myXVel, myYVel, mySize){
 
   this.x = myX;
   this.y = myY;
@@ -48,10 +48,10 @@ function Square(myX, myY, myXVel, myYVel, mySize){
 
   this.move = function(){
     this.x += this.xvel;
-     if(this.x >= width){
+     if(this.x >= width || this.x<= 0){
     
 
-      this.x = 0;
+      this.xvel *= -1;
     }
 
 
@@ -59,10 +59,10 @@ function Square(myX, myY, myXVel, myYVel, mySize){
     this.y += this.yvel;
 
     
-    if(this.y >= height){
+    if(this.y >= height|| this.y<= 0){
     
 
-      this.y = 0;
+      this.yvel *= -1;
     }
 
     this.size += this.sizeStep;
@@ -77,7 +77,7 @@ function Square(myX, myY, myXVel, myYVel, mySize){
     noStroke();
 
     fill(190,30,180);
-    rect(this.x, this.y, this.size, this.size);
+    ellipse(this.x, this.y, this.size, this.size);
 
   }
 
